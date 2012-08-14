59dea538725b37cbb9196e21394b1366e5b651b8
missvchen <missvictoriachen@gmail.com>
2012-08-14 12:54:48 -0400

    fixed database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,29 @@
 
 ActiveRecord::Schema.define(:version => 1) do
 
+  create_table "seamless", :primary_key => "VendorLocationId", :force => true do |t|
+    t.integer "VendorId",                           :null => false
+    t.string  "MarketName",                         :null => false
+    t.string  "VendorName",                         :null => false
+    t.string  "LocationName",                       :null => false
+    t.string  "isactive",           :limit => 1,    :null => false
+    t.string  "Address_Address1",                   :null => false
+    t.string  "Address_City"
+    t.string  "Address_State",      :limit => 3,    :null => false
+    t.string  "Address_ZipCode",    :limit => 10,   :null => false
+    t.string  "Vendor_PhoneNumber", :limit => 45,   :null => false
+    t.string  "DirectUrl",          :limit => 2000, :null => false
+    t.string  "MobileUrl",          :limit => 2000, :null => false
+  end
+
+  add_index "seamless", ["Address_Address1"], :name => "IX_Address_Street"
+  add_index "seamless", ["Address_City"], :name => "IX_Address_City"
+  add_index "seamless", ["Address_State"], :name => "IX_Address_State"
+  add_index "seamless", ["Address_ZipCode"], :name => "IX_Address_ZipCode"
+  add_index "seamless", ["VendorId"], :name => "IX_VendorId"
+  add_index "seamless", ["VendorName"], :name => "IX_VendorName"
+  add_index "seamless", ["isactive"], :name => "IX_isactive"
+
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
