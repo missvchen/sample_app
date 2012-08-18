6620ece3be6d8208974d08a78fffda2211602d40
missvchen <missvictoriachen@gmail.com>
2012-08-18 09:15:45 -0400

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
   has_secure_password
                     
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
   
   #field validation
   validates :name,  presence: true,
@@ -28,4 +29,9 @@ class User < ActiveRecord::Base
   validates :password, length:   { minimum: 6 }
                        
   validates :password_confirmation, presence: true
+  
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 1) do
+ActiveRecord::Schema.define(:version => 2) do
 
   create_table "seamless", :primary_key => "VendorLocationId", :force => true do |t|
     t.integer "VendorId",                           :null => false
@@ -42,8 +42,10 @@ ActiveRecord::Schema.define(:version => 1) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
